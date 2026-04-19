e8b26ecc63cfae00f9e7cc1e0f507b91ca85c34d
RossellaCarone <ro200047@gmail.com>
2026-04-19 23:43:19 +0200

    Fix build paths for GitHub Pages

--- ladolcesosta/astro.config.mjs
+++ ladolcesosta/astro.config.mjs
@@ -3,8 +3,11 @@ import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
 
 export default defineConfig({
-  base: '/ladolcesosta',
   output: 'static',
+  site: 'https://rossellacarone.github.io/ladolcesosta',
+  build: {
+    assets: 'assets',
+  },
   integrations: [tailwind(), preact()],
   vite: {
     server: {
