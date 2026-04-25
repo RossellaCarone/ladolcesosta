560e20dd301679e931ebbca7ba229d7dfd16b221
RossellaCarone <ro200047@gmail.com>
2026-04-25 19:20:38 +0200

    refactor

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,19 +3,10 @@ import tailwind from '@astrojs/tailwind';
 import preact from '@astrojs/preact';
 
 export default defineConfig({
-  base: '/',
   output: 'static',
   site: 'https://ladolcesostahome.com',
-  build: {
-    assets: 'assets',
-  },
-  integrations: [tailwind(), preact()],
-  vite: {
-    server: {
-      watch: {
-        usePolling: true,
-        interval: 500,
-      },
-    },
-  },
+  integrations: [
+    tailwind(),
+    preact(),
+  ],
 });
