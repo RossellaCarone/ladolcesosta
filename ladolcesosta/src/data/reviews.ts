510a03dc219384df34d471fe2a0c50e490acdf6e
RossellaCarone <ro200047@gmail.com>
2026-04-25 15:44:19 +0200

    adding reviews

--- ladolcesosta/src/data/reviews.ts
+++ ladolcesosta/src/data/reviews.ts
@@ -1,7 +1,7 @@
 export interface Review {
   text: string;
   author: string;
-  source: 'Booking.com' | 'Google';
+  source: 'Booking.com' | 'Google' | 'Airbnb';
   rating: number;
   date: string;
 }
@@ -12,6 +12,20 @@ export const reviews: Review[] = [
     author: "Francesco",
     source: "Booking.com",
     rating: 10,
+    date: "2025"
+  },
+  {
+    text: "Alloggio perfetto, pulitissimo e dotato di tutti i comfort, facilità di parcheggio nelle vicinanze e host molto disponibile e cordiale.",
+    author: "Marco",
+    source: "Airbnb",
+    rating: 5,
+    date: "2025"
+  },
+  {
+    text: "Posto molto bello e pulito. Ottima comunicazione con l'host. Consigliatissimo.",
+    author: "Griselda",
+    source: "Airbnb",
+    rating: 5,
     date: "2025"
   },
 ];
